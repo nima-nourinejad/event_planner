d39f10679623a5d90efb10f7294b5cc0ce3907e4
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-07-28 06:02:10 -0400

    paractice

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <section>
+                <p>Address:</p>
+            </section>
+            <section>
+                <p>Phone:</p>
+            </section>
+            
+    </footer>
+    );
+};
+
+export default Footer;
